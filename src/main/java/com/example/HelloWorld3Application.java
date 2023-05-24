73ae26888cdc33bee238a1e26c0c2e55af52ab47
igamasayuki <igamasayuki@gmail.com>
2023-05-24 16:04:23 +0900

    warの設定

--- src/main/java/com/example/HelloWorld3Application.java
+++ src/main/java/com/example/HelloWorld3Application.java
@@ -2,12 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorld3Application {
+public class HelloWorld3Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorld3Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HelloWorld3Application.class);
 	}
 
 }
